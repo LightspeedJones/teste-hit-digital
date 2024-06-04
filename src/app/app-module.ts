4a8c1516b348eda8e8d1fa39f2c6a1b08ff76523
lightspeedjones <felipe.squeiroz@hotmail.com>
2024-06-04 03:55:19 -0300

    criação dos componentes

---
+++ src/app/app-module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from "@angular/core";
+import { HeaderComponent } from "./header/header.component";
+import { IntroducaoComponent } from "./introducao/introducao.component";
+import { AppComponent } from "./app.component";
+import { RouterOutlet } from "@angular/router";
+import { BrowserModule } from "@angular/platform-browser";
+import { ContatoComponent } from "./contato/contato.component";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    IntroducaoComponent,
+    ContatoComponent
+  ],
+  imports:[
+    RouterOutlet,
+    BrowserModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule{}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
+// import { IntroducaoComponent } from './introducao/introducao.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
   title = 'teste-hit-digital';
+  introducao: boolean = true
+  contatos: boolean = false
+
+  public exibirIntroducao(){
+    this.introducao = true;
+    this.contatos = false;
+  }
+
+  public exibirContatos(){
+    this.contatos = true;
+    this.introducao = false;
+  }
 }
